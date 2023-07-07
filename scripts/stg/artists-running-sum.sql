783dab8f5bee08e55f89d3f64b9ca36cdffebefd
Sergio Sánchez Zavala <sergio@cimarron.io>
2023-07-06 21:16:56 -0700

    Update artists-running-sum.sql

--- scripts/stg/artists-running-sum.sql
+++ scripts/stg/artists-running-sum.sql
@@ -2,7 +2,7 @@ create view if not exists stg__artists_running_sum as
 select
     main_artist_id,
     played_date,
-    sum(daily_sum) over (
+    max(daily_sum) over (
         partition by main_artist_id
         order by
             played_date asc
