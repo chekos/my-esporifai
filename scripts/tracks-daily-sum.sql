eca4d9aa5e88f136c56c20119e909f74ab4298d3
Sergio Sánchez Zavala <sergio@cimarron.io>
2023-07-07 16:19:06 -0700

    Create tracks-daily-sum.sql

---
+++ scripts/tracks-daily-sum.sql
@@ -0,0 +1,23 @@
+create view if not exists tracks_daily_sum as
+select
+  tinfo.artist
+  tinfo.album,
+  tinfo.name,
+  hs.played_date,
+  hs.n,
+  hs.id as track_id,
+  tinfo.main_artist_id as artist_id
+  tinfo.album_id
+from
+  tracks_info as tinfo
+  join (
+    select
+      id,
+      date(played_at, "-8 hours") as played_date,
+      count(*) as n
+    from
+      history
+    group by
+      1,
+      2
+  ) as hs on hs.id = tinfo.id
