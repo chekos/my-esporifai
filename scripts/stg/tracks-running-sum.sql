5b71495f94d373f598365e436db882be132b0e2f
Sergio Sánchez Zavala <sergio@cimarron.io>
2023-07-06 21:17:26 -0700

    Update tracks-running-sum.sql

--- scripts/stg/tracks-running-sum.sql
+++ scripts/stg/tracks-running-sum.sql
@@ -2,7 +2,7 @@ create view if not exists stg__tracks_running_sum as
 select
     id,
     played_date,
-    sum(daily_sum) over (
+    max(daily_sum) over (
         partition by id
         order by
             played_date asc
