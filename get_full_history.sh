23a3aa5466ee505fd0e789e5ee21d2810405c51b
chekos <sergio@cimarron.io>
2023-07-13 16:26:10 -0700

    cleanup full-history

--- get_full_history.sh
+++ get_full_history.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-jq --compact-output --sort-keys '[ .[] | select( .spotify_track_uri != null ) | select(.spotify_track_uri | contains("spotify:track")) | { "id": (.spotify_track_uri | ltrimstr("spotify:track")), "played_at": .ts}]' full-history/MyData/streaming_history.json > streaming_history.json
+jq --compact-output --sort-keys '[ .[] | select( .spotify_track_uri != null ) | select(.spotify_track_uri | contains("spotify:track")) | { "id": (.spotify_track_uri | ltrimstr("spotify:track:")), "played_at": .ts}]' full-history/MyData/streaming_history.json > streaming_history.json
 
-sqlite-utils insert spotify.db history streaming_history.json --pk id --pk played_at --ignore
+sqlite-utils insert spotify.db streaming_history streaming_history.json --pk id --pk played_at --ignore
+
+sqlite-utils insert spotify.db full_history full-history/MyData/streaming_history.json --pk ts --ignore
